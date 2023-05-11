c9523c25811bb4ccaf4426ec8e3cd5ac293cfffd
ואדים יאמפולסקי <93340664+VadimYamp@users.noreply.github.com>
2023-05-12 00:33:25 +0300

    ש
    ש

---
+++ Tehilim_119/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Tehilim_119
+{
+    partial class Screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Screen));
+            this.L0 = new System.Windows.Forms.Label();
+            this.L1 = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // L0
+            // 
+            resources.ApplyResources(this.L0, "L0");
+            this.L0.ForeColor = System.Drawing.Color.White;
+            this.L0.Name = "L0";
+            // 
+            // L1
+            // 
+            resources.ApplyResources(this.L1, "L1");
+            this.L1.ForeColor = System.Drawing.Color.White;
+            this.L1.Name = "L1";
+            // 
+            // Timer
+            // 
+            this.Timer.Enabled = true;
+            this.Timer.Interval = 1;
+            this.Timer.Tick += new System.EventHandler(this.T);
+            // 
+            // Screen
+            // 
+            this.AllowDrop = true;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.L1);
+            this.Controls.Add(this.L0);
+            this.DoubleBuffered = true;
+            this.HelpButton = true;
+            this.KeyPreview = true;
+            this.Name = "Screen";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label L0;
+        private System.Windows.Forms.Label L1;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
